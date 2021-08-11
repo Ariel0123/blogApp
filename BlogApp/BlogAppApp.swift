fcb700f2fe4900f5e6e656fbd0cbd0ca0d6e237e
ariel0123 <llamas0123@hotmail.com>
2021-08-11 01:36:28 -0700

    Initial Commit

---
+++ BlogApp/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  BlogApp
+//
+//  Created by Ariel Ortiz on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
